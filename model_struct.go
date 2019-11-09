003b353144c7b64df9bd923977035df6e9fc9d93
Sachi Sangwan <37876508+sachinsangwanjr@users.noreply.github.com>
2019-11-09 14:42:10 +0530

    Update model_struct.go

--- model_struct.go
+++ model_struct.go
@@ -14,6 +14,7 @@ import (
 
 // DefaultTableNameHandler default table name handler
 var DefaultTableNameHandler = func(db *DB, defaultTableName string) string {
+	defaultTableName := "ATF_"+defaultTableName
 	return defaultTableName
 }
 
